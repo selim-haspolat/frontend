30daef2ce1edf9c1a494854867501b4873c0eaf7
selim-haspolat <selimhaspolat@gmail.com>
2024-09-25 18:59:01 +0300

    about update

--- frontend/app/about/page.js
+++ frontend/app/about/page.js
@@ -6,7 +6,7 @@ import teamImage2 from "@/assets/undraw_Team_up_re_84ok.png";
 const AboutPage = () => {
   return (
     <div className="grid grid-cols-4 grid-rows-3 gap-4">
-      <div className="col-span-2 row-span-1 flex flex-col gap-2">
+      <div className="col-span-2 row-span-1 flex flex-col justify-end gap-4">
         <h1 className="text-8xl font-semibold">About Us</h1>
         <div className="border"></div>
         <p>
@@ -20,10 +20,10 @@ const AboutPage = () => {
       <div className="col-span-2 row-span-2">
         <Image height={10000} width={10000} className="w-fit" src={teamImage} />
       </div>
-      <div className="col-span-2 row-span-2 flex flex-col gap-4">
+      <div className="col-span-2 row-span-2">
         <Image height={10000} width={10000} className="w-fit" src={teamImage2} />
       </div>
-      <div className="col-span-2 row-span-1 flex flex-col justify-end gap-2">
+      <div className="col-span-2 row-span-1 flex flex-col gap-4">
         <h1 className="text-8xl font-semibold">Team</h1>
         <div className="border"></div>
         <p>
